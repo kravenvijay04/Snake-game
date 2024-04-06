db4c5cfc034c05d347cf16333e8f5b8879d71a66
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-04-07 00:22:16 +0530

    Snakefood function is added

--- action.js
+++ action.js
@@ -1,9 +1,23 @@
 const gameboard = document.getElementById('gameboard');
 const context = gameboard.getContext('2d');
 
+let WIDTH=gameboard.width;
+let HEIGHT=gameboard.height;
+let UNIT=25;
 startgame();
 
 function startgame(){
-    context.fillStyle='black';
-    context.fillRect(0,0,500,500)
+    context.fillStyle='#212121';
+    context.fillRect(0,0,WIDTH,HEIGHT)
+    createfood();
+    displayfood();
+}
+function createfood(){
+Xfood=Math.floor(Math.random()*WIDTH/UNIT)*UNIT;
+Yfood=Math.floor(Math.random()*HEIGHT/UNIT)*UNIT;
+}
+
+function displayfood(){
+    context.fillStyle='red';
+    context.fillRect(Xfood,Yfood,UNIT,UNIT);
 }
