cc73c9ea31086ed131ad65960055cb94374ef10b
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-04-11 00:06:43 +0530

    Function for Snake movement is added

--- action.js
+++ action.js
@@ -6,6 +6,8 @@ let HEIGHT=gameboard.height;
 let UNIT=25;
 let Xfood;
 let Yfood;
+let Xvel=25;
+let Yvel=0;
 
 let snake=[
     {x:UNIT*3,y:0},
@@ -20,8 +22,14 @@ function startgame(){
     context.fillRect(0,0,WIDTH,HEIGHT)
     createfood();
     displayfood();
-    drawsnake();
+    Timemovement();
 }
+
+function clearboard(){
+    context.fillStyle='#212121';
+    context.fillRect(0,0,WIDTH,HEIGHT)
+}
+
 function createfood(){
 Xfood=Math.floor(Math.random()*WIDTH/UNIT)*UNIT;
 Yfood=Math.floor(Math.random()*HEIGHT/UNIT)*UNIT;
@@ -39,4 +47,23 @@ function drawsnake(){
         context.fillRect(snakepart.x,snakepart.y,UNIT,UNIT)
         context.strokeRect(snakepart.x,snakepart.y,UNIT,UNIT)
     })
+}
+
+function movesnake(){
+    let head={
+        x:snake[0].x+Xvel,
+        y:snake[0].y+Yvel
+    }
+    snake.unshift(head);
+    snake.pop();
+}
+
+function Timemovement(){
+    setTimeout(()=>{
+    clearboard();
+    displayfood();
+    movesnake();
+    drawsnake();
+    Timemovement();
+    },100)
 }
