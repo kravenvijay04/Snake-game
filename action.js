078d5660c5a420a3fcace26e3d5416cd4ba94089
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-04-10 23:37:44 +0530

    Function for creating snake is added

--- action.js
+++ action.js
@@ -4,6 +4,15 @@ const context = gameboard.getContext('2d');
 let WIDTH=gameboard.width;
 let HEIGHT=gameboard.height;
 let UNIT=25;
+let Xfood;
+let Yfood;
+
+let snake=[
+    {x:UNIT*3,y:0},
+    {x:UNIT*2,y:0},
+    {x:UNIT,y:0},
+    {x:0,y:0}
+]
 startgame();
 
 function startgame(){
@@ -11,6 +20,7 @@ function startgame(){
     context.fillRect(0,0,WIDTH,HEIGHT)
     createfood();
     displayfood();
+    drawsnake();
 }
 function createfood(){
 Xfood=Math.floor(Math.random()*WIDTH/UNIT)*UNIT;
@@ -20,4 +30,13 @@ Yfood=Math.floor(Math.random()*HEIGHT/UNIT)*UNIT;
 function displayfood(){
     context.fillStyle='red';
     context.fillRect(Xfood,Yfood,UNIT,UNIT);
+}
+
+function drawsnake(){
+    context.fillStyle="aqua"
+    context.strokeStyle="black"
+    snake.forEach((snakepart)=>{
+        context.fillRect(snakepart.x,snakepart.y,UNIT,UNIT)
+        context.strokeRect(snakepart.x,snakepart.y,UNIT,UNIT)
+    })
 }
